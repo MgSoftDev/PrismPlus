e55bf5d36dbb59a3d26f3162016de436dd3cae22
Miguel Fernando garcia Salazar <migeru_garcia@hotmail.com>
2023-08-05 17:25:26 -0600

    Add DelegateCommandExtension functionality
    This commit introduces a new static class, `DelegateCommandExtension`, in the `MgSoftDev.PrismPlus.Helper.Extension` namespace. This class provides extension methods like `CanAndExecute` for standard `DelegateCommand` and `AsyncDelegateCommand` classes. These methods will check if the commands can be executed and if so, they execute them. The addition of these methods simplifies coordination of command executability checks and actual command execution.
    
    Took 10 minutes

---
+++ MgSoftDev.PrismPlus/Helper/Extension/DelegateCommandExtension.cs
@@ -0,0 +1,32 @@
+﻿using MgSoftDev.PrismPlus.Commands;
+using Prism.Commands;
+
+namespace MgSoftDev.PrismPlus.Helper.Extension;
+
+public static class DelegateCommandExtension
+{
+    public static bool CanAndExecute(this DelegateCommand command)
+    {
+        var res = command.CanExecute();
+        if (res)command.Execute();
+        return res;
+    }
+    public static bool CanAndExecute<T>(this DelegateCommand<T> command, T parameter)
+    {
+        var res = command.CanExecute(parameter);
+        if (res)command.Execute(parameter);
+        return res;
+    }
+    public static bool CanAndExecute(this AsyncDelegateCommand command)
+    {
+        var res = command.CanExecute();
+        if (res)command.Execute();
+        return res;
+    }
+    public static bool CanAndExecute<T>(this AsyncDelegateCommand<T> command, T parameter)
+    {
+        var res = command.CanExecute(parameter);
+        if (res)command.Execute(parameter);
+        return res;
+    }
+}
